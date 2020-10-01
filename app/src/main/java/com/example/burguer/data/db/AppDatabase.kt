3e5b62323257601274b6cef293cd2233a7b48f0b
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-10-01 09:21:00 -0300

    MenuDao

--- app/src/main/java/com/example/burguer/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/burguer/data/db/AppDatabase.kt
@@ -1,7 +1,33 @@
 package com.example.burguer.data.db
-
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-// Classe paro banco de dados
+
+// Classe para o banco de dados
+
+@Database(entities = [MenuEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase(){
 
+    companion object {
+        // Singleton prevents multiple instances of database opening at the same time.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/burguer/data/db/dao/MenuDao.kt
@@ -0,0 +1,19 @@
+package com.example.burguer.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.burguer.data.db.MenuEntity
+
+@Dao
+interface MenuDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(menu: MenuEntity)
+
+    @Query("SELECT * FROM menu WHERE id = :id")
+    fun getMenu(id: Long) : MenuEntity
+
+    
+}
